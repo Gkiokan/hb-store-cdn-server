51ec984c01a102322861e0909a048977fff8e02f
Gkiokan Sali <gkiokan@hotmail.de>
2022-04-04 22:46:02 +0200

    create base project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-black">
+      <q-toolbar>
+
+        <q-toolbar-title class="text-subtitle1">
+          HB-Store Local CDN Server
+        </q-toolbar-title>
+
+        <div>v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+
+        The initial center :)
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
