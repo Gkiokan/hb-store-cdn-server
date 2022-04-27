b733d387ed86b170e5ca1bcd48bcfc6189c35a1d
Gkiokan Sali <gkiokan@hotmail.de>
2022-04-27 20:15:41 +0200

    update button state to disabled based on the server staet

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -39,9 +39,9 @@
     <q-btn class="full-width q-mb-md" :color="getServerStateColor" :label="getServerStateLabel" />
 
     <div class='q-gutter-md space-around'>
-        <q-btn outline icon="play_arrow" color="green-8" label="Start" @click="$root.startServer" v-if="!state || state == 'stopped'" />
-        <q-btn outline icon="restart_alt" color="orange-8" label="Restart" @click="$root.restartServer" v-if="state == 'running'" />
-        <q-btn outline icon="stop" color="red-8" label="Stop" @click="$root.stopServer" v-if="state == 'running'" />
+        <q-btn outline icon="play_arrow" color="green-8" label="Start" @click="$root.startServer" :disable="!state || state != 'stopped'" />
+        <q-btn outline icon="restart_alt" color="orange-8" label="Restart" @click="$root.restartServer" :disable="state != 'running'" />
+        <q-btn outline icon="stop" color="red-8" label="Stop" @click="$root.stopServer" :disable="state != 'running'" />
     </div>
 
     <q-separator class="q-my-md" v-if="true" />
