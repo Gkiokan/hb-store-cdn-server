f63bbfeccfbfc0c1c11397ce90bd368b661ec29f
Gkiokan Sali <gkiokan@hotmail.de>
2022-04-28 23:11:42 +0200

    fix start button state../.. once again

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -39,7 +39,7 @@
     <q-btn class="full-width q-mb-md" :color="getServerStateColor" :label="getServerStateLabel" />
 
     <div class='q-gutter-md space-around'>
-        <q-btn outline icon="play_arrow" color="green-8" label="Start" @click="$root.startServer" :disable="!!(state != null | state != 'stopped')" />
+        <q-btn outline icon="play_arrow" color="green-8" label="Start" @click="$root.startServer" :disable="startButtonState" />
         <q-btn outline icon="restart_alt" color="orange-8" label="Restart" @click="$root.restartServer" :disable="state != 'running'" />
         <q-btn outline icon="stop" color="red-8" label="Stop" @click="$root.stopServer" :disable="state != 'running'" />
     </div>
@@ -101,6 +101,12 @@ export default {
               return 'black'
 
             return 'pink'
+        },
+
+        startButtonState(){
+            if(this.state == null) return false
+            if(this.state == 'stopped') return false
+            return true
         },
     },
 
